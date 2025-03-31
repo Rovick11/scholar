59e8d8112b52c701a3102f5399b8253339337769
annehilarie3 <annehilarie.agawa03@gmail.com>
2025-03-31 14:10:16 +0800

    update the design of personal information of the user

--- resources/views/userdash.blade.php
+++ resources/views/userdash.blade.php
@@ -93,122 +93,155 @@
 <div id="popup" class="popup">
     <div class="popup-content">
         <span class="close" onclick="closePopup()">&times;</span>
-        <h2>Personal Information</h2>
+        <h2> </h2>
         <form id="updateForm" action="{{ route('userprofile.update', ['id' => $user->id ?? 0]) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT') <!-- Use PUT for updating data -->
 
-            <div class="input-group">
-                <label for="last-name">Last Name</label>
-                <input type="text" id="last-name" name="last_name" value="{{ $user->lastName }}" required>
-            </div>
+        <fieldset>
+        <legend>Personal Information</legend>
+        <div class="form-container">
+        <div class="input-group">
+            <input type="text" id="last-name" name="last_name" class="small-input" value="{{ $user->lastName }}" required placeholder=" ">
+            <label for="last-name">Last Name</label>
+        </div>
 
-            <div class="input-group">
-                <label for="first-name">First Name</label>
-                <input type="text" id="first-name" name="first_name" value="{{ $user->firstName }}" required>
-            </div>
-
-            <div class="input-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" value="{{ $user->email }}" required>
-            </div>
-
-            <div class="input-group">
-                <label for="phone">Phone Number</label>
-                <input type="text" id="phone" name="phone" value="{{ $user->contactNo }}" required>
-            </div>
-
-            <div class="input-group">
-                <label for="birth-date">Birth Date</label>
-                <input type="date" id="birth-date" name="birth_date" value="{{ $user->birthDate }}" required>
-            </div>
-            <div class="input-group">
-            <label for="barangay">Barangay</label>
+        <div class="input-group">
+            <input type="text" id="first-name" name="first_name" class="small-input" value="{{ $user->firstName }}" required placeholder=" ">
+            <label for="first-name">First Name</label>
+        </div>
+        
+        <div class="input-group">
+            <input type="text" id="middle-initial" name="middle_initial" class="small-input" value="{{ $user->middleInitial }}" required placeholder=" ">
+            <label for="middle-initial">Middle Initial</label>
+        </div>
+        
+        <div class="input-group">
+            <input type="email" id="email" name="email" class="small-input" value="{{ $user->email }}" required placeholder=" ">
+            <label for="email">Email</label>
+        </div>
+        
+        <div class="input-group">
+            <input type="text" id="phone" name="phone" class="small-input" value="{{ $user->contactNo }}" required placeholder=" ">
+            <label for="phone">Phone Number</label>
+        </div>
+        
+        <div class="input-group">
+            <input type="date" id="birth-date" name="birth_date" class="small-input" value="{{ $user->birthDate }}" required>
+            <label for="birth-date">Birth Date</label>
+        </div>
+        
+        <div class="input-group">
             <select id="barangay" name="barangay" required>
-                                <option value="{{ $user->barangay }}">{{ $user->barangay }}</option>
-                                <option value="Aga">Aga</option>
-                                <option value="Balaytigui">Balaytigui</option>
-                                <option value="Banilad">Banilad</option>
-                                <option value="Barangay 1">Barangay 1</option>
-                                <option value="Barangay 2">Barangay 2</option>
-                                <option value="Barangay 3">Barangay 3</option>
-                                <option value="Barangay 4">Barangay 4</option>
-                                <option value="Barangay 5">Barangay 5</option>
-                                <option value="Barangay 6">Barangay 6</option>
-                                <option value="Barangay 7">Barangay 7</option>
-                                <option value="Barangay 8">Barangay 8</option>
-                                <option value="Barangay 9">Barangay 9</option>
-                                <option value="Barangay 10">Barangay 10</option>
-                                <option value="Barangay 11">Barangay 11</option>
-                                <option value="Barangay 12">Barangay 12</option>
-                                <option value="Bilaran">Bilaran</option>
-                                <option value="Bucana">Bucana</option>
-                                <option value="Bulihan">Bulihan</option>
-                                <option value="Bunducan">Bunducan</option>
-                                <option value="Butucan">Butucan</option>
-                                <option value="Calayo">Calayo</option>
-                                <option value="Catandaan">Catandaan</option>
-                                <option value="Kaylaway">Kaylaway</option>
-                                <option value="Kayrilaw">Kayrilaw</option>
-                                <option value="Cogunan">Cogunan</option>
-                                <option value="Dayap">Dayap</option>
-                                <option value="Latag">Latag</option>
-                                <option value="Looc">Looc</option>
-                                <option value="Lumbangan">Lumbangan</option>
-                                <option value="Malapad na Bato">Malapad na Bato</option>
-                                <option value="Mataas na Pulo">Mataas na Pulo</option>
-                                <option value="Maugat">Maugat</option>
-                                <option value="Munting Indan">Munting Indan</option>
-                                <option value="Natipuan">Natipuan</option>
-                                <option value="Pantalan">Pantalan</option>
-                                <option value="Papaya">Papaya</option>
-                                <option value="Putat">Putat</option>
-                                <option value="Reparo">Reparo</option>
-                                <option value="Talangan">Talangan</option>
-                                <option value="Tumalim">Tumalim</option>
-                                <option value="Utod">Utod</option>
-                                <option value="Wawa">Wawa</option>
-                            </select>
+                <option value="{{ $user->barangay }}">{{ $user->barangay }}</option>
+                <option value="Aga">Aga</option>
+                <option value="Balaytigui">Balaytigui</option>
+                <option value="Banilad">Banilad</option>
+                <option value="Barangay 1">Barangay 1</option>
+                <option value="Barangay 2">Barangay 2</option>
+                <option value="Barangay 3">Barangay 3</option>
+                <option value="Barangay 4">Barangay 4</option>
+                <option value="Barangay 5">Barangay 5</option>
+                <option value="Barangay 6">Barangay 6</option>
+                <option value="Barangay 7">Barangay 7</option>
+                <option value="Barangay 8">Barangay 8</option>
+                <option value="Barangay 9">Barangay 9</option>
+                <option value="Barangay 10">Barangay 10</option>
+                <option value="Barangay 11">Barangay 11</option>
+                <option value="Barangay 12">Barangay 12</option>
+                <option value="Bilaran">Bilaran</option>
+                <option value="Bucana">Bucana</option>
+                <option value="Bulihan">Bulihan</option>
+                <option value="Bunducan">Bunducan</option>
+                <option value="Butucan">Butucan</option>
+                <option value="Calayo">Calayo</option>
+                <option value="Catandaan">Catandaan</option>
+                <option value="Kaylaway">Kaylaway</option>
+                <option value="Kayrilaw">Kayrilaw</option>
+                <option value="Cogunan">Cogunan</option>
+                <option value="Dayap">Dayap</option>
+                <option value="Latag">Latag</option>
+                <option value="Looc">Looc</option>
+                <option value="Lumbangan">Lumbangan</option>
+                <option value="Malapad na Bato">Malapad na Bato</option>
+                <option value="Mataas na Pulo">Mataas na Pulo</option>
+                <option value="Maugat">Maugat</option>
+                <option value="Munting Indan">Munting Indan</option>
+                <option value="Natipuan">Natipuan</option>
+                <option value="Pantalan">Pantalan</option>
+                <option value="Papaya">Papaya</option>
+                <option value="Putat">Putat</option>
+                <option value="Reparo">Reparo</option>
+                <option value="Talangan">Talangan</option>
+                <option value="Tumalim">Tumalim</option>
+                <option value="Utod">Utod</option>
+                <option value="Wawa">Wawa</option>
+            </select>
+            <label for="barangay">Barangay</label>
+        </div>
+        
+        <div class="input-group">
+            <select id="sex" name="sex" required>
+                <option value="{{ $user->sex }}">{{ $user->sex }}</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+            <label for="sex">Sex</label>
+        </div>
+    </fieldset>
+
+    <!-- Educational Information -->
+    <fieldset>
+        <legend>Educational Information</legend>
+        <div class="form-container">
+        <div class="input-group">
+            <input type="text" id="university" name="university" class="small-input" value="{{ $user->university }}" required placeholder=" ">
+            <label for="university">University</label>
+        </div>
+        <div class="input-group">
+            <input type="text" id="year" name="year" class="small-input" value="{{ $user->year }}" required placeholder=" ">
+            <label for="year">Year</label>
+        </div>
+        <div class="input-group">
+            <input type="text" id="semester" name="semester" class="small-input" value="{{ $user->semester }}" required placeholder=" ">
+            <label for="semester">Semester</label>
+        </div>
+        <div class="input-group">
+            <input type="text" id="course" name="course" class="small-input1" value="{{ $user->course }}" required placeholder=" ">
+            <label for="course">Course</label>
+        </div>
+    </fieldset>
+
+    <!-- Account Security -->
+    <fieldset>
+    <legend>Account Security</legend>
+    <div class="form-container1">
+        <div class="input-group1">
+            <div class="otp-container">
+                <input type="text" id="otp" name="otp" maxlength="6" placeholder=" " disabled>
+                <label for="otp">Enter OTP</label>
+                <button type="button" class="btn btn-secondary" id="sendOtp">Send OTP</button>
             </div>
-            <div class="input-group">
-                            <label for="sex">Sex
-                            </label>
-                            <select id="sex" name="sex" style="width: 110px;">
-                                <option value="{{ $user->sex}}">{{ $user->sex }}</option>
+            <span id="otpStatus" class="text-danger"></span>
+        </div>
 
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-            </div>
-
-            
-          
-
-            <!-- OTP Input -->
+        <div class="password-container">
             <div class="input-group1">
-                <label for="otp" class="form-label">Enter OTP</label>
-                <input type="text" id="otp" name="otp" class="form-control" maxlength="6" disabled>
-                <span id="otpStatus" class="text-danger"></span> <!-- Display OTP validation -->
-                <button type="button" class="btn btn-secondary" id="sendOtp">Send otp</button>
-                
+                <input type="password" id="password" name="password" placeholder=" " disabled>
+                <label for="password">New Password</label>
             </div>
 
-          
-
-            <!-- New Password Fields -->
-            <div class="input-group">
-                <label for="password" class="form-label">New Password</label>
-                <input type="password" id="password" name="password" class="form-control" disabled>
+            <div class="input-group1">
+                <input type="password" id="password_confirmation" name="password_confirmation" placeholder=" " disabled>
+                <label for="password_confirmation">Confirm Password</label>
             </div>
+        </div>
+    </div>
+</fieldset>
+    
+    <button type="submit">Update</button>
+</form>
 
-            <div class="input-group">
-                <label for="password_confirmation" class="form-label">Confirm Password</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" disabled>
-            </div>
-
-
-            <button type="submit">Update</button>
-        </form>
 
     </div>
 </div>
